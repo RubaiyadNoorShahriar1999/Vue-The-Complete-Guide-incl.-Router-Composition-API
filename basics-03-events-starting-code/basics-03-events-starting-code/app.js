d939cfcbf7da633fdc191ebffd37880ac27de2a0
RubaiyadNoorShahriar1999 <rubaiyadnoorshahriar@gmail.com>
2023-05-25 17:15:11 +0600

    Update till video 25: vue attributes (v-html,v-on:click, v-on:input), accessing target object using default function pointing on input event

--- basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -1,9 +1,33 @@
+// use , to add more methods/properties
+
 const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
+  },
+  methods: {
+    // Again this keyword is used to access the data of the Vue instance
+
+    add(num) {
+      // this.counter++;
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      // this.counter--;
+      this.counter = this.counter - num;
+    },
+
+    // This event is a default parameter which will take the values of the event from the browser. We can use this to access the value of the input field. We don't need to send this event parameter from the HTML code.
+    setValue1(event) {
+      this.name = event.target.value;
+    },
+
+    setValue2(event, secondName) {
+      this.name = event.target.value + " " + secondName;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
